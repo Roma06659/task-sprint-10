16a100eba50d4672ae9aefb4f1025a6f3e5edcf9
Roma Tereshenko <Tri.89@mail.ru>
2024-03-28 00:57:12 +0500

    added task

--- task.sh
+++ task.sh
@@ -16,9 +16,7 @@ touch dir2/empty
 # создаём файл task/dir2/hello.sh с таким содержанием:
 # #!/bin/bash
 # echo "$1, привет!"
-touch dir2/hello.sh
-echo '#!/bin/bash
-echo "$1, Привет!" ' >> dir2/hello.sh
+echo '#!/bin/bash\n echo "$1, Привет!"' > dir2/hello.sh
 
 # устанавливаем для task/dir2/hello.sh права rwxrw-r--
 chmod 764 dir2/hello.sh
@@ -27,7 +25,7 @@ chmod 764 dir2/hello.sh
 ls dir2 > dir2/list.txt
 
 # копируем содержимое каталога task/dir2 в каталог task/dir3/dir4
-cp -r -T dir2 dir3/dir4
+cp -r dir2 dir3/dir4
 
 # записываем в task/dir1/summary.txt список файлов с расширением *.txt
 # находящихся в task, включая поддиректории
@@ -37,7 +35,7 @@ find ./ -name "*.txt" > dir1/summary.txt
 cat dir2/list.txt >> dir1/summary.txt
 
 # определяем переменную окружения NAME со значением "Всем студентам"
-NAME="Всем студентам!"
+NAME='Всем студентам'
 
 # запускаем task/dir2/hello.sh с переменной окружения NAME в качестве аргумента
 # вывод скрипта должен дописаться в файл task/dir1/summary.txt
